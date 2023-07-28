628028811ac292ebb55ddf23c17b4f34b3156ce7
Joseph Montalto <joey.montalto@hotmail.com>
2023-07-27 23:45:11 -0700

    Updated Orphan Program 07.27.23

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use std::error::Error;
 use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
 use regex::Regex;
-use std::time::Duration;
-use std::threat::sleep;
+use regex::Captures;
+use chrono::{DateTime, Utc, TimeZone};
+use std::time;
+// use std::fs::File; // This is for testing
+
+const TIME_CONSTRAINT: u64 = 5;
 
 /*******************************  main  *******************************
  *
@@ -26,7 +30,7 @@ async fn main()
 
         
         /* The loop to detect orphaned blocks */
-        match orphan_detection() {
+        match time_checker().await {
                 Ok(_) => println!("Extraction completed."),
                 Err(e) => {
                         eprintln!("An error occurred: {}", e);
@@ -38,17 +42,12 @@ async fn main()
                         return;
                 }
         }
-        
-        /* Find blockroot information */
-        match verify_blockroot() {
-                Ok(_) => println!("Extraction completed."),
-                Err(e) =>  {
-                        eprintln!("Err occurred in find_validator: {}", e);
-                        let err_msg = format!("Program is closing due to error in find_validator as per the following: {}", e);
-                        match send_to_slack(&err_msg).await {
-                            Ok(_) => println!("Err message relayed to Slack. "),
-                            Err(e) => eprintln!("Err message could not be sent to Slack: {}", e),
-                        }
+
+        /* Send closing message to Slack */
+        match send_to_slack("Orphan program is closing.").await {
+                Ok(_) => println!("Connection ending with Slack environment."),
+                Err(e) => {
+                        eprintln!("An error occurred in send_to_slack: {}", e);
                         return;
                 }
         }
@@ -61,8 +60,9 @@ async fn main()
  * Panics: None
  * Errors: Will throw error if webhook link or msg are invalid
  */
-async fn send_to_slack(msg: &str) -> Result<(), Box<dyn Error>> {
-        let webhook_url = "https://hooks.slack.com/services/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";
+async fn send_to_slack(msg: &str) -> Result<(), Box<dyn Error>> 
+{
+        let webhook_url = "https://hooks.slack.com/services/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";
         let client = reqwest::Client::new();
         let res = client.post(webhook_url)
                 .json(&serde_json::json!({
@@ -75,55 +75,151 @@ async fn send_to_slack(msg: &str) -> Result<(), Box<dyn Error>> {
         Ok(())
 }
 
-
-/* Name: orphan_detection
+/* Name: time_checker
  * Input: None
- * Panics: None
+ * Panics: Checks on Regex lines and line parsing within BufReader object.
  * Errors: Will throw error if any of the log information is formatted 
  *         different than expected.
- * 
- * 
  */
-
- // This function has many bugs and is not complete.
-fn orphan_detection() -> Result<(), Box<dyn Error>> {
+ async fn time_checker() -> Result<(), Box<dyn Error>> 
+ {
         let mut child = Command::new("journalctl")
                 .arg("-f")
                 .stdout(Stdio::piped())
                 .spawn()?;
-
         let reader = BufReader::new(child.stdout.take().unwrap());
 
-        let re_proposal = Regex::new(r"timeTillDuty=([\d\w]+).*slot=(\d+)")?;
-        let re_submission = Regex::new(r#"time="(\d+-\d+-\d+ \d+:\d+:\d+)" level=info msg="Submitted new block""#)?;
+        // Uncomment the file below for testing.
+        // let file = File::open("src/validator_logs.txt")?;
+        // let reader = BufReader::new(file); 
 
-        for line_result in reader.lines() {
-                let line = line_result?;
-                if let Some(captures) = re_proposal.captures(&line) {
-                        let time_till_duty = &captures[1];
-                        println!("Found timeTillDuty: {}", time_till_duty);
-                } else if let Some(captures) = re_submission.captures(&line) {
-                        let submission_time = &captures[1];
-                        let slot_id = &captures[2].parse()?;
-                        verify_blockroot(block_root)?;
-                        println!("Found submission time: {}", submission_time);
-                        println!("Found block_root: {}", block_root);
+        // Initialize desired expression formats and data vectors.
+        let re_proposal = Regex::new(r#"time="(\d+-\d+-\d+ \d+:\d+:\d+)".*level=info.*msg="Proposal schedule".*pubKey=(0x[\da-fA-F]+).*slot=(\d+).*timeTillDuty=([\d\w]+)"#)?;
+        let re_submission = Regex::new(r#"time="(\d+-\d+-\d+ \d+:\d+:\d+)".*level=info.*msg="Submitted new block""#)?;
+        let mut time_limits: Vec<(String, DateTime<Utc>, String)> = Vec::new();
+        let mut submissions: Vec<DateTime<Utc>> = Vec::new();
+
+        
+        let mut case_entered = false;
+        for line in reader.lines() {
+                let line = line?;
+                if re_proposal.is_match(&line) { // If a proposal is detected
+                        let captures = Some(re_proposal.captures(&line).unwrap());
+                        add_proposal(captures, &mut time_limits);
+                        case_entered = true;
+                } else if re_submission.is_match(&line) { // If a submission is detected
+                        let captures = Some(re_submission.captures(&line).unwrap());
+                        add_submission(captures, &mut submissions);
+                        case_entered = true;
+                }
+                if case_entered {
+                        check_times(&mut time_limits, &mut submissions).await;
+                        case_entered = false;
                 }
         }
-
         Ok(())
 }
 
 
-
-/* Name: verify_blockroot
- * Input: None
+/* Name: check_times
+ * Input: The vectors that contain time limit and submission information.
  * Panics: None.
- * Errors: Will throw error if the journalctl logs do not contain 
- *         the pubKey variable.
+ * Errors: There will be unexpected behavior if the submissions vector happens 
+ *         to be longer than the time_limits, or if the elements within the 
+ *         vectors are misaligned.
  */
- fn verify_blockroot() -> Result<(), Box<dyn Error>> {
-        let url = "https://beaconcha.in/api/v1/slot/6919473";
-        let response = request::get(url).await?.json().await?;
-        Ok(response)
+ async fn check_times(time_limits: &mut Vec<(String, DateTime<Utc>, String)>, submissions: &mut Vec<DateTime<Utc>>) 
+ {
+        let mut to_remove = Vec::new();
+        for (i, (time_limit, submission)) in time_limits.iter().zip(submissions.iter()).enumerate() {
+                if time_limit.1 > *submission {
+                        to_remove.push(i);
+                } else if time_limit.1 <= *submission {
+                        let error_msg = format!("There has been a potentially orphaned block at slot {} by validator {}.\nThe time limit was {}, but has been submitted at {}", time_limit.0.clone(), time_limit.2.clone(), time_limit.1, submission);
+                        match send_to_slack(&error_msg).await {
+                                Ok(_) => println!("Sending an error to Slack regarding validator orphan risk with {}.", time_limit.2.clone()),
+                                Err(e) => {
+                                        eprintln!("An error occurred in send_to_slack: {}", e);
+                                        return;
+                                }
+                        }
+                        to_remove.push(i);
+                }
+        }
+        to_remove.reverse();
+        for i in to_remove {
+            time_limits.remove(i);
+            submissions.remove(i);
+        }
+    }
+    
+
+
+/* Name: add_proposal
+ * Input: A Captures object of the date and time_limits vector by reference.
+ * Panics: None.
+ * Errors: None.
+ */
+fn add_proposal(captures: Option<Captures>, time_limits: &mut Vec<(String, DateTime<Utc>, String)>) 
+{
+        match captures {
+            Some(captures) => {
+                // Calculate the time limit and push onto the vector 
+                // with the corresponding slot #
+                let time_limit = get_time_limit(&captures[1], &captures[4]);
+                time_limits.push((captures[3].to_string(), time_limit, captures[2].to_string()));
+            },
+            None => {
+                println!("No captures found for this proposal.");
+            }
+        }
+}
+
+
+
+/* Name: add_submission
+ * Input: A Captures object of the date and submissions vector by reference.
+ * Panics: None.
+ * Errors: None.
+ */
+fn add_submission(captures: Option<Captures>, submissions: &mut Vec<DateTime<Utc>>) 
+{
+        match captures {
+                Some(captures) => {
+                        // Redefine the string as a date object and push 
+                        // onto vector.
+                        let date = Utc.datetime_from_str(&captures[1], "%Y-%m-%d %H:%M:%S").unwrap();
+                        submissions.push(date);
+                },
+                None => {
+                        println!("No captures found for this proposal.");
+                }
+        } 
+}
+
+
+
+/* Name: get_time_limit
+ * Input: A string of the time, and the allocated time for completion.
+ * Panics: None.
+ * Errors: There will be unexpected behavior if the string time is incorrectly
+ *         formatted.
+ */
+fn get_time_limit(time: &str, time_till_duty: &str) -> DateTime<Utc> {
+        let time = Utc.datetime_from_str(time, "%Y-%m-%d %H:%M:%S").unwrap();
+        
+        let duration = if time_till_duty.len() > 3 {
+            let re_duration = Regex::new(r"(\d+)m(\d+)s").unwrap();
+            let captures = re_duration.captures(time_till_duty).unwrap();
+            let minutes = captures.get(1).unwrap().as_str().parse::<u64>().unwrap();
+            let seconds = captures.get(2).unwrap().as_str().parse::<u64>().unwrap();
+            time::Duration::from_secs(minutes * 60 + seconds + TIME_CONSTRAINT)
+        } else {
+            let re_duration = Regex::new(r"(\d+)s").unwrap();
+            let captures = re_duration.captures(time_till_duty).unwrap();
+            let seconds = captures.get(1).unwrap().as_str().parse::<u64>().unwrap();
+            time::Duration::from_secs(seconds + TIME_CONSTRAINT)
+        };
+        
+        time + chrono::Duration::from_std(duration).unwrap()
 }
